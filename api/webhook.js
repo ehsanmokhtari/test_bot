21452f310332fdbce2cd0d143d13bf838aa8210b
Ehsan Mokhtari <e69m91@gmail.com>
2024-10-11 13:06:13 +0330

    init

---
+++ api/webhook.js
@@ -0,0 +1,42 @@
+// Require our Telegram helper package
+const Telegraf = require('telegraf');
+
+// Export as an asynchronous function
+// We'll wait until we've responded to the user
+module.exports = async (request, response) => {
+    try {
+        // Create our new bot handler with the token
+        // that the Botfather gave us
+        // Use an environment variable so we don't expose it in our code
+        const bot = new Telegraf(process.env.BOT_TOKEN);
+
+        // Retrieve the POST request body that gets sent from Telegram
+        const { body } = request;
+
+        // Ensure that this is a message being sent
+        if (body.message) {
+            // Retrieve the ID for this chat
+            // and the text that the user sent
+            const { chat: { id }, text } = body.message;
+
+            // Create a message to send back
+            // We can use Markdown inside this
+            const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+
+            // Send our new message back in Markdown and
+            // wait for the request to finish
+            await bot.on("text", ctx => ctx.reply(message));
+        }
+    }
+    catch (error) {
+        // If there was an error sending our message then we 
+        // can log it into the Vercel console
+        console.error('Error sending message');
+        console.log(error.toString());
+    }
+
+    // Acknowledge the message with Telegram
+    // by sending a 200 HTTP status code
+    // The message here doesn't matter.
+    response.send('OK');
+};
